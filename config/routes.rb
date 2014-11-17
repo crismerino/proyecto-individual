e88f94391a6588b433b8dd13292eea9f92ec78d1
Cristina <crismerinom@yahoo.es>
2014-11-17 14:22:45 +0100

    html de index hecho, con css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    get 'home' => 'home#portada'
+    get 'registro' => 'home#registro'
+    get 'desing_Ruta' => 'home#desing_Ruta'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
