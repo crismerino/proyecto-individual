ab4e4b0fca136deaddc0049429e1a385ffee7295
Cristina <crismerinom@yahoo.es>
2014-11-18 13:09:01 +0100

    drag and drop working

--- app/assets/javascripts/desing_Ruta.js
+++ app/assets/javascripts/desing_Ruta.js
@@ -7,24 +7,36 @@ function inicio() {
     document.getElementById('recuadro').addEventListener('dragover', permitirDrop, false);        
     document.getElementById('recuadro').addEventListener('drop', drop, false);
     document.getElementById('recuadro').addEventListener('drop', drop, false);
-    drag.addEventListener('dragend', handleDragEnd, false);                
+//    document.getElementById('foto1').addEventListener('dragend', soltar, false);   
+//    document.getElementById('foto2').addEventListener('dragend', soltar, false);  
+//    document.getElementById('foto3').addEventListener('dragend', soltar, false);                 
 }
+
 
 function drag(ev)
 {
     ev.dataTransfer.setData("src",ev.target.id);
+    var miDiv = document.getElementById('recuadro');
+    miDiv.classList.add('cambiar');
+    console.log("drag");
 }    
-
+function permitirDrop(ev)
+{
+    ev.preventDefault();
+    console.log("permite log");
+}
+//function soltar(ev){
+//    var miDiv = document.getElementById('recuadro');
+//    miDiv.classList.remove('cambiar');
+//}
 function drop(ev)
 {
     ev.preventDefault();
     var dato=ev.dataTransfer.getData("src");
     ev.target.appendChild(document.getElementById(dato));
+    console.log("drop");
 }  
 
 
-function permitirDrop(ev)
-{
-    ev.preventDefault();
-}
+
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    get 'home' => 'home#portada'
+    root 'home#portada'
     get 'registro' => 'home#registro'
     get 'desing_Ruta' => 'home#desing_Ruta'
 
