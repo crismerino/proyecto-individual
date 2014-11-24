ef4382ddaccdcf4bf4177008aef44efd906a5186
Cristina <crismerinom@yahoo.es>
2014-11-24 18:54:55 +0100

    changed some styles

--- app/assets/javascripts/desing_Ruta.js
+++ app/assets/javascripts/desing_Ruta.js
@@ -3,7 +3,6 @@ var sacaModal = document.getElementsByClassName('fotos')[0].children;
 for (i=0; i<sacaModal.length; i++){
   sacaModal[i].addEventListener('click', openModal);
 };
-
 function openModal (event){
         $("#myModal").modal('show');
 };
